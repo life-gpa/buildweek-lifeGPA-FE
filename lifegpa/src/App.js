afee7f4b87de2f4c4c7ca9210be64b20ec7c0cfb
kbrian97 <ktorgbede2015@fau.edu>
2019-05-01 21:39:16 -0400

    LaunchPage

--- lifegpa/src/App.js
+++ lifegpa/src/App.js
@@ -1,22 +1,20 @@
-import React, { Component } from 'react';
-import { Route} from "react-router-dom";
+import React from 'react';
+import { Route } from "react-router-dom";
 import Login from './Components/Login';
 import Signup from './Components/Signup';
 import HomePage from './Components/Homepage';
-import StartPage from './Components/StartPage';
+import LaunchPage from './Components/LaunchPage';
 
-class App extends Component {
-
-  render() {
+function App() {
     return (
         <div className="App">
-          <Route path="/" exact render={props => <StartPage {...props}/>} />
+          <Route path="/" exact render={props => <LaunchPage {...props}/>} />
           <Route path="/signup" render={props => <Signup {...props} />} />
           <Route path="/login" render={props => <Login {...props} />} />
           <Route path="/home" render={props => <HomePage {...props} />} />
         </div>
     );
   }
-}
+
 
 export default App;
--- lifegpa/src/Components/LaunchPage.js
+++ lifegpa/src/Components/LaunchPage.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import {Link} from 'react-router-dom'
 
-export default class StartPage extends Component {
+export default class LaunchPage extends Component {
   render() {
     return (
     <div>
